4fa1ba97304ffc9c5300bc4cc3878bb346c559ae
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-22 06:35:42 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,18 @@ name = input("What's your name? ")
 #Remove white space from str
 name = name.strip()
 
+#Capitalize users name 
+name = name.capitalize()
+
+#Capitalize users first and last name
+name = name.title()
+
+# Remove white space and capitalize
+name = name.strip().title() 
+
+# Ask user for users name, remove white space, and capitalize
+name = input("What's your name? ").strip().title()
+
 # Say hello to user
 print ("hello,")
 print (name)
