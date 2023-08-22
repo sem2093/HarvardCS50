2a3f0d16fad3a42d30a6b2f97821316b2a21910e
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-22 06:10:25 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,5 +3,13 @@ name = input("What's your name? ")
 # Say hello to user
 print ("hello,")
 print (name)
+# another way to say hello
+print (f"hello {name}")
+# and another way
+print ("hello, " +name)
+# and another way
+print ("hello,", name)
+
+
 
 
