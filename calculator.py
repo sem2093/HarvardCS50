690690fa7167e0a8373a0808fded405a2f7c7242
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-22 07:29:12 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,18 @@
-
+#Calculator ex 1
 x = 1
 y = 2 
-
 z = x + y 
 print(z) 
 
+#Calculator ex 2
 # Ask user for x and y values.
 x = input("What's x? ")
 y = input("What's y? ")
 # Add and convert x and y from string to integer 
 z = int(x) + int(y) 
 print(z) 
+
+#Calculator ex 3 
+def main() :
+.... x = int(input("what's x? "))
+.... 
