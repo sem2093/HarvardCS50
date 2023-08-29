7313d4277ad8ac1d4ff44d24e3bafeb8223f1c31
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-29 15:23:41 -0600

    Update grade.py

--- lecture_notes/grade.py
+++ lecture_notes/grade.py
@@ -10,4 +10,27 @@ elif score >= 60 and score < 70:
     print("Grade: D")
 else:
     print("Grade: F")
-
+    
+#simplified
+if 90 <= score <= 100:
+    print("Grade: A")
+elif 80<= score < 90:
+    print("Grade: B")
+elif 70 <= score < 80:
+    print("Grade: C")
+elif 60 <= score < 70:
+    print("Grade: D")
+else:
+    print("Grade: F")
+    
+#futher simplified 
+if score >=90:
+    print("Grade: A")
+elif score >=80:
+    print("Grade: B")
+elif score >=70:
+    print("Grade: C")
+elif score >=60:
+    print("Grade: D")
+else: 
+    print("Grade: F)
