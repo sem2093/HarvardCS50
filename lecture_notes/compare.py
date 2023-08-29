138f742fd63465aaa6c823cb266544683f509415
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-29 14:51:18 -0600

    Update compare.py

--- lecture_notes/compare.py
+++ lecture_notes/compare.py
@@ -7,3 +7,25 @@ if x > y:
     print("x is greater than y")
 if x == y:
     print("x is equal to y")
+
+# "elif" only runs if prior condition is false 
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
+
+# "else" runs if prior conditions are false
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
+# "or" combines conditions
+if x < y or x > y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+    
