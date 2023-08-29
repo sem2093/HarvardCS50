937e0cdd0fa3427e316de393528298f3627bc5bb
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-29 15:53:58 -0600

    Update parity.py

--- lecture_notes/parity.py
+++ lecture_notes/parity.py
@@ -1,6 +1,23 @@
-x = int(input("What's x? ")
+x = int(input("What's x? "))
 
 if x % 2 == 0 :
     print("Even")
 else:
     print("Odd")
+    
+# using functions 
+# define main function
+def main():
+    x = int(input("What's x?"))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+# define is_even and return bool
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+# call main function
+main()
