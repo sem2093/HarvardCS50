63533c8e6e9184c09e78d862351c75eb22191d8f
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-29 17:15:23 -0600

    Create cat.py

---
+++ lecture_notes/cat.py
@@ -0,0 +1,25 @@
+#print meow three times 
+print("Meow")
+print("Meow")
+print("Meow")
+
+#using while loops
+i = 3
+while i != 0:
+    print("Meow")
+    i = i - 1
+#or 
+i = 0 
+while i < 3:
+    print("Meow")
+    i += 1 
+# using for loops
+for i in [0,1,2]: 
+    print("Meow")
+# using range 
+for i in range(3): 
+    print("Meow")
+#or use _ to say variable isnt important
+for _ in range(3): 
+    print("Meow")
+
