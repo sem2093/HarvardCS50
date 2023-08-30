041b4b2f0455bd0f0ee06c8c0c54b9d677c8a99d
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 11:13:54 -0600

    Create gryffindors.py

---
+++ lecture_notes/gryffindors.py
@@ -0,0 +1,14 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+]
+
+gryffindors = []
+for student in students:
+    if student["house"] == "Gryffindor":
+        gryffindors.append(student["name"])
+
+for gryffindor in sorted(gryffindors):
+    print(gryffindor)
