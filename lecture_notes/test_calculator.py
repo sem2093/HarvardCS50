2b763331d387a647894d8349393c5c193d0a6337
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 07:30:01 -0600

    Update test_calculator.py

--- lecture_notes/test_calculator.py
+++ lecture_notes/test_calculator.py
@@ -41,3 +41,20 @@ def test_assert():
     assert square(-2) == 4
     assert square(-3) == 9
     assert square(0) == 0
+
+# divide code into different groups of tests
+from calculator import square
+
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+
+def test_zero():
+    assert square(0) == 0
