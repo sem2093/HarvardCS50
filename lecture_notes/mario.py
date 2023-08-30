942a4e51978045e782e9851622b79b9290fe10c0
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 03:15:02 -0600

    Update mario.py

--- lecture_notes/mario.py
+++ lecture_notes/mario.py
@@ -60,3 +60,16 @@ def print_row(width):
     print("#" * width)
 
 main()
+
+# create a pyramid 
+
+def main(): 
+    height = int(input("height: ")) 
+    pyramid(height)
+
+def pyramid(n):
+    for i in range(n):
+        print("#" * (i + 1))
+
+if __name__ == "__main__":
+    main()
