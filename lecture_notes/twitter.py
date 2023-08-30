be088d1a3afb572f60f478403559cbef8015de81
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 09:51:59 -0600

    Update twitter.py

--- lecture_notes/twitter.py
+++ lecture_notes/twitter.py
@@ -1,2 +1,56 @@
 url = input("URL: ").strip()
 print(url)
+
+# add replace and username
+url = input("URL: ").strip()
+
+username = url.replace("https://twitter.com/", "")
+print(f"Username: {username}")
+
+# utilize "removeprefix"
+url = input("URL: ").strip()
+
+username = url.removeprefix("https://twitter.com/")
+print(f"Username: {username}")
+
+# implement re.sub method 
+import re
+
+url = input("URL: ").strip()
+
+username = re.sub(r"https://twitter.com/", "", url)
+print(f"Username: {username}")
+
+#add "^"
+import re
+
+url = input("URL: ").strip()
+
+username re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
+print(f"Username: {username}")
+
+# add re.search
+import re
+
+url = input("URL: ").strip()
+
+matches = re.search(r"^https?://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
+if matches:
+    print(f"Username:", matches.group(2))
+
+# add ":="
+import re
+
+url = input("URL: ").strip()
+
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+    print(f"Username:", matches.group(1))
+
+# adding [a-z0-9_]
+import re
+
+url = input("URL: ").strip()
+
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
+    print(f"Username:", matches.group(1))
+
