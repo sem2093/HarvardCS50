87f860469ec4bb862fa53f69db7c1cd98448b495
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 08:28:08 -0600

    Update students.py

--- lecture_notes/students.py
+++ lecture_notes/students.py
@@ -2,3 +2,73 @@ with open("students.csv") as file:
     for line in file:
         row = line.rstrip().split(",")
         print(f"{row[0]} is in {row[1]}")
+
+# simplified 
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
+
+# sort output
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        students.append(f"{name} is in {house}")
+
+for student in sorted(students):
+    print(student)
+
+# use dictionary to sort 
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {}
+        student["name"] = name
+        student["house"] = house
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
+    
+# improved minus sorting 
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
+
+# sort list of dictionaries by students name 
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        students.append({"name": name, "house": house})
+
+
+def get_name(student):
+    return student["name"]
+
+
+for student in sorted(students, key=get_name):
+    print(f"{student['name']} is in {student['house']}")
+
+# simplified 
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        students.append({"name": name, "house": house})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['house']}")
