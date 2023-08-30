43a3476b27452a4238b7f3443ca5214340fe19c3
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 10:34:14 -0600

    Create type.py

---
+++ lecture_notes/type.py
@@ -0,0 +1,11 @@
+print(type(50))
+#apply to string
+print(type("hello, world"))
+#apply to list
+print(type([]))
+#built in list class
+print(type(list()))
+#apply to dict
+print(type({}))
+#built in dict class 
+print(type(dict()))
