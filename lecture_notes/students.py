e129c1b2f144bc97441c1054870a0d625bf8fff9
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 08:34:26 -0600

    Update students.py

--- lecture_notes/students.py
+++ lecture_notes/students.py
@@ -72,3 +72,28 @@ with open("students.csv") as file:
 
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
+
+# change from house to home 
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, home = line.rstrip().split(",")
+        students.append({"name": name, "home": home})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['home']}")
+
+#use csv reader 
+import csv
+
+students = []
+
+with open("students.csv") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        students.append({"name": row[0], "home": row[1]})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
+
