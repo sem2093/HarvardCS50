d46581d8027f38bd457c99b8ce81aceef4a43845
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 04:07:18 -0600

    Update say.py

--- lecture_notes/say.py
+++ lecture_notes/say.py
@@ -1,6 +1,13 @@
 import cowsay
 import sys
 
+# have a cow say hello
+
 if len(sys.argv) == 2:
     cowsay.cow("hello, " + sys.argv[1])
+
+# have a trex say hello
+
+if len(sys.argv) == 2:
+    cowsay.trex("hello, " + sys.argv[1])
   
