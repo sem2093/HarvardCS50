81c10bd6c857c84067376053cfa9064aa4988670
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-29 18:00:08 -0600

    Update cat.py

--- lecture_notes/cat.py
+++ lecture_notes/cat.py
@@ -22,4 +22,51 @@ for i in range(3):
 #or use _ to say variable isnt important
 for _ in range(3): 
     print("Meow")
+# using multiplication operator
+print("Meow" * 3)
+# ask user how many times cat should meow
+while True: 
+    n = int(input("What's n? "))
+    if n < 0:
+        continue
+    else:
+        break
+# or 
+while True: 
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
+
+
+# defining main function
+def main():
+    meow(3)
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+meow() 
+
+# or 
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number(): 
+    while True: 
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+    return n 
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+meow()
+
+
 
