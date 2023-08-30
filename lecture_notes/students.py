e01e3e76905c59758b18d030711e8eaac88c26ad
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 08:38:54 -0600

    Update students.py

--- lecture_notes/students.py
+++ lecture_notes/students.py
@@ -97,3 +97,25 @@ with open("students.csv") as file:
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is from {student['home']}")
 
+# using DictReader
+import csv
+
+students = []
+
+with open("students.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['home']}")
+
+# write csv file 
+import csv
+
+name = input("What's your name? ")
+home = input("Where's your home? ")
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
