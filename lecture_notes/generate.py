04bbb729950f56eaa63490dc8549954ad626ef14
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 03:31:23 -0600

    Update generate.py

--- lecture_notes/generate.py
+++ lecture_notes/generate.py
@@ -1,1 +1,26 @@
+import random
 
+coin = random.choice(["heads", "tails"])
+print(coin)
+
+# refine import and remove random from random.choice 
+from random import choice
+
+coin = choice(["heads", "tails"])
+print(coin)
+
+# randomly generate a number between 1 and 10
+
+import random
+
+number = random.randint(1, 10)
+print(number)
+
+# shuffle cards
+import random
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
+  
