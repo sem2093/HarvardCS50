193c43d71e70da1b0f85764f63e47144309d9d92
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 07:39:56 -0600

    Update test_hello.py

--- lecture_notes/test_hello.py
+++ lecture_notes/test_hello.py
@@ -5,3 +5,13 @@ def test_hello():
     assert hello("David") == "hello, David"
     assert hello() == "hello, world"
 
+# break down into multiple functions
+from hello import hello
+
+
+def test_default():
+    assert hello() == "hello, world"
+
+
+def test_argument():
+    assert hello("David") == "hello, David"
