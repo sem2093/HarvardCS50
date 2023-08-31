050b615151286dd9b17832c4b8baa1d8b0f4ec42
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-30 23:21:44 -0600

    Update project.py

--- final-project/project.py
+++ final-project/project.py
@@ -1,17 +1,35 @@
 def main():
-    print("Main")
-    function_1()
-    function_2()
-    function_n()
+    print("Welcome to My To-Do List App!")
+    tasks = []
+    
+    while True:
+        print("\n1. Add Task")
+        print("2. View Tasks")
+        print("3. Quit")
+        
+        choice = input("Enter your choice: ")
+        
+        if choice == '1':
+            task = input("Enter a new task: ")
+            tasks.append(task)
+            function_1()
+        elif choice == '2':
+            print("\n--- Your Tasks ---")
+            for index, task in enumerate(tasks, start=1):
+                print(f"{index}. {task}")
+        elif choice == '3':
+            function_n()
+            break
+        else:
+            function_2()
 
 def function_1():
-    print("Function 1")
+    print("Task added successfully!")
 
 def function_2():
-    print("Function 2")
-
+    print("Please Type '1','2',or'3'")
 def function_n():
-    print("Function n")
+    print("Goodbye!")
 
 if __name__ == "__main__":
     main()
